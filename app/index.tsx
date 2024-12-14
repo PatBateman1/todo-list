825c6dadcf516eb2d1d5da1b176cc6898a1b9364
Bateman <zz2700@columbia.edu>
2024-12-14 23:22:12 +0800

    feat: add ui framework

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,18 @@
 import { Text, View } from 'react-native';
 
+import '@/global.css';
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
+
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <GluestackUIProvider mode="light"><View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>Edit app/index.fgfgggfgf to edit this screen.</Text>
+      </View></GluestackUIProvider>
   );
 }
